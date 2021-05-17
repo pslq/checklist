24aaf61133fed058685856f3c1562347d32961f9
Paulo Queiroz <paulo.sergio.lemes.queiroz@gmail.com>
2021-05-17 15:34:12 -0300

    Updated mpstat to properly read pctUtil from the dedicated and shared lpars

--- sh/mpstat.sh
+++ sh/mpstat.sh
@@ -13,48 +13,44 @@ export LANG="C"
 export LC_NUMERIC="POSIX"
 
 
-mpstat -a 1 1 |
+mpstat -a 1 2 |
   awk '{
-    if ( mode == "" )
+    if ( $1 == "System" )
     {
-      if ( $29 == "ilcs" )
-        mode="shr";
-      else
+      if ( $0 ~ /mode=Donating/ ) {
         mode="ded";
-        
-    } else if ( $1 == "ALL" )
-    {
-      count=count+1;
-      v10 = $10+v10; v11 = $11+v11; v13 = $13+v13; v30 = $30+v30;
-      v17 = $17+v17; v18 = $18+v18; v19 = $19+v19; v20 = $20+v20;
-      v21 = $21+v21; v22 = $22+v22; v24 = $24+v24; v25 = $25+v25;
-      v26 = $26+v26; v27 = $27+v27; v28 = $28+v28; v29 = $29+v29;
+      } else if ( $0 ~ /mode=Uncapped/ )
+        mode="shr";
+    } else {
+      if ( $1 == "ALL" )
+      {
+        count=count+1;
+        if ( mode == "shr" )
+        {
+          user = user + $24; sys  = sys + $25; wait = wait + $26;
+          idle = idle + $27; pc   = pc + $28; ec   = ec + $29;
+          ics  = ics + $11; cs   = cs  + $10; rq   = rq + $13;
+          ilcs = ilcs + $30; vlcs = vlcs + $31;
+        } else if ( mode == "ded" ) {
+          user = user + $24; sys  = sys + $25; wait = wait + $26;
+          idle = idle + $27; pc   = pc + $28; ec   =  ec + ( (pc/($24+$25))*100 );
+          ics  = ics + $11; cs   = cs  + $10; rq   = rq + $13;
+          ilcs = ilcs + $29; vlcs = vlcs + $30;
+        }
+      }
     }
   } END {
-    if ( v10 > 0 ) v10=v10/count; if ( v11 > 0 ) v11=v11/count;
-    if ( v13 > 0 ) v13=v13/count; if ( v17 > 0 ) v17=v17/count;
-    if ( v18 > 0 ) v18=v18/count; if ( v19 > 0 ) v19=v19/count;
-    if ( v20 > 0 ) v20=v20/count; if ( v21 > 0 ) v21=v21/count;
-    if ( v22 > 0 ) v22=v22/count; if ( v24 > 0 ) v24=v24/count;
-    if ( v25 > 0 ) v25=v25/count; if ( v26 > 0 ) v26=v26/count;
-    if ( v27 > 0 ) v27=v27/count; if ( v28 > 0 ) v28=v28/count;
-    if ( v29 > 0 ) v29=v29/count; if ( v30 > 0 ) v30=v30/count;
+    pctUser=user/count; pctSystem=sys/count; pctIowait=wait/count;
+    pctIdle=idle/count; pctCore=pc/count;    pctUtil=ec/count;
+    ics = ics/count;    cs = cs/count;       ilcs = ilcs/count;
+    vlcs = vlcs/count;  RunQueue=rq/count;   pc=pc/count;
 
-    if ( v10 > 0 )
-      ilcs_ratio=(v11*100)/v10;
-    else
-      ilcs_ratio=(v11*100);
-    if ( v30 > 0 )
-      vlcs_ratio=(v29*100)/v30;
-    else
-      vlcs_ratio=(v29*100);
+    InvoluntaryContextSwitch=ics/cs;
+    InvoluntaryCoreContextSwitch=ilcs/vlcs;
 
-    core_aff=v17+v18;
-    mcm_aff=v19+v20;
-    poor_aff=v21+v22;
-
-    printf("CPU=all pctUser=%f pctSystem=%f pctIowait=%f pctIdle=%f pctUtil=%f InvoluntaryContextSwitch=%f InvoluntaryCoreContextSwitch=%f RunQueue=%f ent=%f\n",
-    v24,v25,v26,v27,v28,ilcs_ratio,vlcs_ratio,v13,v20);
+    printf("CPU=all pctUser=%f pctSystem=%f pctIowait=%f pctIdle=%f pc=%f pctUtil=%f InvoluntaryContextSwitch=%f InvoluntaryCoreContextSwitch=%f RunQueue=%f\n",
+                    pctUser,   pctSystem,   pctIowait,   pctIdle,   pc,   pctUtil,   InvoluntaryContextSwitch,   InvoluntaryCoreContextSwitch,   rq);
 
   }'
+
 
