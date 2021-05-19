a1fec9feaf65970696a6a0c826831b79c622cf7e
Paulo Queiroz <paulo.sergio.lemes.queiroz@gmail.com>
2021-05-19 08:39:04 -0300

    Added more core consumption options for mpstat.sh added a consolidate data option for lspath ( helpfull with splunk )

--- sh/lspath.sh
+++ sh/lspath.sh
@@ -4,6 +4,8 @@
 # and is licensed at GPLV3
 #
 #####################################################################
+# Report consolidated data only
+consolidate="1"
 
 # Global vars
 export PATH="${PATH}:/usr/lpp/mmfs/bin:/usr/es/sbin/cluster/utilities:/usr/es/sbin/cluster/utilities:/usr/es/sbin/cluster/sbin"
@@ -11,7 +13,7 @@ export PATH="${PATH}:/usr/lib/ras:/usr/sbin/rsct/bin:/opt/NetApp/santools/bin:/u
 export LC_NUMERIC="POSIX"
 
 
-lspath | awk '
+lspath | awk -v consolidate="${consolidate}" '
 {
   if ( $1 == "Enabled" )
     enabled[$2]++;
@@ -23,27 +25,36 @@ lspath | awk '
   adpt_path[$3]++;
 
 } END {
+  asym_dsk_path = 0;
+  dead_dsk_path = 0;
+  tot_adpt_path = 0;
+  tot_adpt      = 0;
+  asy_adpt      = 0;
+
   for ( dsk in enabled )
   {
     if ( dsk % 2 != 0 )
     {
-      print("ERROR=\"Odd amount of paths for "dsk"\"");
+      if ( consolidate != 1 )
+        print("ERROR=\"Odd amount of paths for "dsk"\"");
+      asym_dsk_path= asym_dsk_path+1;
     }
     n = split(dsk_apt[dsk],P_ADPT," ");
     if ( n < 2 )
     {
-      print("ERROR=\"Asymmetrical amount of paths for "dsk"\"");
+      if ( consolidate != 1 )
+        print("ERROR=\"Asymmetrical amount of paths for "dsk"\"");
+      asym_dsk_path= asym_dsk_path+1;
     }
 
   }
   for ( dsk in other )
   {
-    print("ERROR=\"Dead path at "dsk"\"");
+    if ( consolidate != 1 )
+      print("ERROR=\"Dead path at "dsk"\"");
+    dead_dsk_path= dead_dsk_path+1;
   }
 
-  tot_adpt_path = 0;
-  tot_adpt = 0;
-  asy_adpt = 0;
   for ( adpt in adpt_path )
   {
     if ( tot_adpt_path == 0 )
@@ -53,12 +64,10 @@ lspath | awk '
         asy_adpt = 1;
     tot_adpt++;
   }
-  if ( asy_adpt == 1 )
-  {
+  if (( asy_adpt == 1 )&&( consolidate != 1 ))
     print("ERROR=\"Asymmetrical Path distribution across the controllers\"");
-  }
-  if ( tot_adpt < 2 )
-  {
+  if (( tot_adpt < 2 )&&( consolidate != 1 ))
     print("ERROR=\"Only one disk controller found\"");
-  }
+
+  printf("asy_disk_path=%d dead_disk_path=%d asy_adpt=%d tot_adpt=%d\n", asym_dsk_path, dead_dsk_path, asy_adpt, tot_adpt);
 }'
--- sh/mpstat.sh
+++ sh/mpstat.sh
@@ -4,39 +4,55 @@
 # and is licensed at GPLV3
 #
 #####################################################################
-
-
+# Report pctUtil based on ent% cores or online cpus ?
+rep_mode="ec" # <ec|vcpu> # report based only on ec%
+# if set to "ec", will use only the ec% field to report data ( can go beyond 100% )
+# if set to "vcpu" will use the amountof cpus are assigned to the lpar + ec% to caculate total amount that the lpar can get to itself
+##
+count=2    # amount of mpstat samples to read
+interval=1 # interval between the samples
+#####################################################################
 # Global vars
-export PATH="${PATH}:/usr/lpp/mmfs/bin:/usr/es/sbin/cluster/utilities:/usr/es/sbin/cluster/utilities:/usr/es/sbin/cluster/sbin"
-export PATH="${PATH}:/usr/lib/ras:/usr/sbin/rsct/bin:/opt/NetApp/santools/bin:/usr/DynamicLinkManager/bin:/usr/es/sbin/cluster/diag:/usr/ios/cli"
 export LANG="C"
 export LC_NUMERIC="POSIX"
 
 
-mpstat -a 1 2 |
-  awk '{
-    if ( $1 == "System" )
+mpstat -a ${interval} ${count} |
+  awk -v rep_mode="${rep_mode}" -v lpar_data="$(lparstat -i |  awk \
+  '{
+    if ( $1 == "Type" ) {
+      if ( $3 ~ /Shared/ )
+        mode = "shr";
+      else
+        mode = "ded";
+    } else if ( $0 ~ /Online Virtual CPUs/ ) {
+      ent_cpu = $5;
+    } else if (( $0 ~ /Entitled Capacity/ )&&( $0 !~ /Pool/ )) {
+      ent_core = $4;
+    }
+  } END {
+    print(mode,ent_cpu,ent_core);
+  }')" '{
+    split(lpar_data,lpd," ");
+    if ( $1 == "ALL" )
     {
-      if ( $0 ~ /mode=Donating/ ) {
-        mode="ded";
-      } else if ( $0 ~ /mode=Uncapped/ )
-        mode="shr";
-    } else {
-      if ( $1 == "ALL" )
+      count=count+1;
+      if ( lpd[1] == "shr" )
       {
-        count=count+1;
-        if ( mode == "shr" )
-        {
-          user = user + $24; sys  = sys + $25; wait = wait + $26;
-          idle = idle + $27; pc   = pc + $28; ec   = ec + $29;
-          ics  = ics + $11; cs   = cs  + $10; rq   = rq + $13;
-          ilcs = ilcs + $30; vlcs = vlcs + $31;
-        } else if ( mode == "ded" ) {
-          user = user + $24; sys  = sys + $25; wait = wait + $26;
-          idle = idle + $27; pc   = pc + $28; ec   =  ec + ( (pc/($24+$25))*100 );
-          ics  = ics + $11; cs   = cs  + $10; rq   = rq + $13;
-          ilcs = ilcs + $29; vlcs = vlcs + $30;
+        user = user + $24; sys  = sys + $25; wait = wait + $26;
+        idle = idle + $27; pc   = pc + $28;
+        ics  = ics + $11; cs   = cs  + $10; rq   = rq + $13;
+        ilcs = ilcs + $30; vlcs = vlcs + $31;
+        if ( rep_mode == "ec" )
+          ec   = ec + $29;
+        else {
+          ec = ec + ( (lpd[3]*$29)/lpd[2]);
         }
+      } else if ( lpd[1] == "ded" ) {
+        user = user + $24; sys  = sys + $25; wait = wait + $26;
+        idle = idle + $27; pc   = pc + $28; ec   =  ec + ( (pc/($24+$25))*100 );
+        ics  = ics + $11; cs   = cs  + $10; rq   = rq + $13;
+        ilcs = ilcs + $29; vlcs = vlcs + $30;
       }
     }
   } END {
@@ -44,13 +60,8 @@ mpstat -a 1 2 |
     pctIdle=idle/count; pctCore=pc/count;    pctUtil=ec/count;
     ics = ics/count;    cs = cs/count;       ilcs = ilcs/count;
     vlcs = vlcs/count;  RunQueue=rq/count;   pc=pc/count;
-
     InvoluntaryContextSwitch=ics/cs;
     InvoluntaryCoreContextSwitch=ilcs/vlcs;
-
     printf("CPU=all pctUser=%f pctSystem=%f pctIowait=%f pctIdle=%f pc=%f pctUtil=%f InvoluntaryContextSwitch=%f InvoluntaryCoreContextSwitch=%f RunQueue=%f\n",
                     pctUser,   pctSystem,   pctIowait,   pctIdle,   pc,   pctUtil,   InvoluntaryContextSwitch,   InvoluntaryCoreContextSwitch,   rq);
-
   }'
-
-
