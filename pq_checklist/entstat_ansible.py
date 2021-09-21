111ee302e6992c453009325b050b97b0f9c2120b
Paulo Queiroz <paulo.sergio.lemes.queiroz@gmail.com>
2021-09-21 07:03:27 -0300

    added entstat ansible interface

---
+++ pq_checklist/entstat_ansible.py
@@ -0,0 +1,63 @@
+#!/opt/freeware/bin/python3
+
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '0.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r'''
+  Simple module to get entstat for all interfaces on the server
+'''
+
+EXAMPLES = r'''
+  No parameter is needed
+'''
+
+RETURN = r'''
+  Return a list of adapters and a dict with all stats
+'''
+
+def main():
+  from ansible.module_utils.basic import AnsibleModule
+  import os
+  module = AnsibleModule( supports_check_mode=False, argument_spec = {})
+  result = {
+      'changed' : False,
+      'rc' : 0,
+      'stats' : {},
+      'adapters' : []
+      }
+
+  if 'aix' in os.sys.platform :
+    # Get Adapter list
+    rc, stdout, stderr = module.run_command("lsdev -C -H -S a -F name:class:subclass:type")
+    if rc == 0 :
+      for l in stdout.split('\n') :
+        lp = l.strip().split(':')
+        if len(lp) == 4 :
+          if any([ True for st in ( 'en', 'ent' ) if st in lp[0]]) and lp[1] in ( "if", "adapter" ) :
+            result['adapters'].append(lp[0])
+
+    # Get Stats for these adapters
+    for adpt in result['adapters'] :
+      rc, stdout, stderr = module.run_command("entstat -d %s"%adpt)
+      if rc == 0 :
+        result['stats'][adpt] = stdout
+    if len(result['stats']) == 0 :
+      result['msg'] = 'No adapter with stats found'
+      result['rc'] = -1
+  else :
+    result['rc'] = -1
+
+  # Return data
+  if result['rc'] != 0 :
+    module.fail_json(**result)
+  else :
+    result['changed'] = True
+    result['msg'] = 'ok'
+  module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
