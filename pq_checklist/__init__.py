ad694e20898b1b47889f6cf805524f5ce44e768d
Paulo Queiroz <paulo.sergio.lemes.queiroz@gmail.com>
2021-12-15 15:07:07 -0300

    Fixed another typo into round_list function

--- pq_checklist/__init__.py
+++ pq_checklist/__init__.py
@@ -39,7 +39,7 @@ def pq_round_number(number,ndigits=2,tp=float) :
 #######################################################################################################################
 def get_list_avg_and_diff(lst:list, calculate_diff:bool=True) :
   if calculate_diff :
-    just_changes = [ v if p == 0 else v-x[p-1] for p,v in enumerate(lst) ]
+    just_changes = [ v if p == 0 else v-lst[p-1] for p,v in enumerate(lst) ]
   else :
     just_changes = lst
   change_rate = pq_round_number([ True if p == 0 else True if v-just_changes[p-1] == 0 else False for p,v in enumerate(just_changes) ].count(False)/len(just_changes))
